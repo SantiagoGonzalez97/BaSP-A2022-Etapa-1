3a6a0b3e98fd451dddac9a5817b5c7fb10e90f6a
SantiagoGonzalez97 <santiagogonzalez97@hotmail.com>
2022-09-15 20:54:32 -0300

    BaSP-W04-A2022: First block of exercise done

---
+++ Semana-04/exercises/01_variables_and_operators.js
@@ -0,0 +1,21 @@
+//1. Variables y Operadores:
+//a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+var firstNumber, secondNumber, suma;
+firstNumber = 5;
+secondNumber= 10;
+suma = firstNumber + secondNumber;
+console.log(suma);
+
+//b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+var firstName, lastName, fullName;
+firstName = 'Santiago ';
+lastName = 'González';
+fullName = firstName + lastName;
+console.log(fullName);
+
+//c)Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+var  firstVar, secondVar, bothVar;
+firstVar = 'Javascript';
+secondVar = 'Exercises';
+bothVar = firstVar.length + secondVar.length;
+console.log(bothVar);
