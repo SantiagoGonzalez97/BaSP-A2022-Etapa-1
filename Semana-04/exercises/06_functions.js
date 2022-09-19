a24f90384cd781aee98a3a6562e70293af18aa97
SantiagoGonzalez97 <santiagogonzalez97@hotmail.com>
2022-09-19 20:44:18 -0300

    BaSP-W04-A2022: Functions done

--- Semana-04/exercises/06_functions.js
+++ Semana-04/exercises/06_functions.js
@@ -9,49 +9,64 @@ console.log('Exercise 6.a): ' + myVarA)
 
 //b) A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros no es un número; de no ser un número, mostrar una alerta aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado.
 function sumaB(a,b) {
-    if (isNaN(sumaB)){
+    if (isNaN(a) || isNaN(b)){
         alert('Exercise 6.b): ' + 'Parameter Error\nIts not a number');
         return NaN;
-    } else {
-        return a + b;
     }
+    return a + b;
 }
 myVarA = sumaB(1,2)
 
 //c) Aparte, crear una función validate Integer que reciba un número como parámetro y devuelva verdadero si es un número entero.
 function validateInteger(i){
     if (Number.isInteger(i)){
-        return console.log('Exercise 6.c): ' + 'The number ' + i + ' its integer');
+        return true;
     }
-    console.log('Exercise 6.c): ' + 'The number ' + i + ' its not integer');
-}
+    return false;
+    }
 i = validateInteger(10)
 
 //d) A la función suma del ejercicio 6b) agregarle una llamada a la función del ejercicio 6c. y que valide que los números sean enteros. En caso que haya decimales mostrar un alerta con el error y retornar el número convertido a entero (redondeado).
-function sumaB(a,b){
-    if (isNaN(sumaB)){
+function sumaD(a,b){
+    if (isNaN(a) || isNaN(b)){
         alert('Exercise 6.d): ' + 'Parameter Error\nIts not a number');
         return NaN;
-    } else if (Number.isInteger(a)){
-        return console.log('Exercise 6.d): ' + 'The number ' + i + ' its integer');
-    } else if (Number.isInteger(a)){
-        console.log('Exercise 6.d): ' + 'The number ' + i + ' its not integer');
     }
-    else if(!NaN){
-        return a + b;
+    if (!validateInteger(a) || !validateInteger(b)) {
+        if (!validateInteger(a)) {
+            alert('Exercise 6.d): ' + 'Parameter Error\nIts not an integer: ' + Math.round(a));
+        }
+        if (!validateInteger(b)) {
+            alert('Exercise 6.d): ' + 'Parameter Error\nIts not an integer: ' + Math.round(b));
+        }
+    } else {
+        return console.log('Exercise 6.d): ' +  (a + b));
     }
 }
-myVarA = sumaB(1,2)
+myVarA = sumaD(1,2)
 
-
-/*if (Number.isInteger(a)) {
-            return console.log('is integer' + a);
+//e) Convertir la validación del ejercicio 6d) en una función separada y llamarla dentro de la función suma probando que todo siga funcionando igual.
+function validateInteger(a,b){
+    if (!validateInteger(a) || !validateInteger(b)){
+        if (!validateInteger(a)) {
+            alert('Exercise 6.e): ' + 'Parameter Error\nIts not an integer: ' + Math.round(a));
         }
-        console.log('isnt integer' + a);
-        if (Number.isInteger(b)) {
-            return console.log('is integer' + b);
+        if (!validateInteger(b)) {
+            alert('Exercise 6.e): ' + 'Parameter Error\nIts not an integer: ' + Math.round(b));
+        }else {
+            return console.log('Exercise 6.e): ' +  (a + b));
         }
     }
-    return a + b;*/
-
-
+    function sumaD(a,b){
+        if (isNaN(a) || isNaN(b)){
+            alert('Exercise 6.e): ' + 'Parameter Error\nIts not a number');
+            return NaN;
+        }
+        else{
+            validateInteger(a);
+            validateInteger(b);
+            return a + b
+        }
+    }
+    myVarA = sumaD(1,2)
+}
