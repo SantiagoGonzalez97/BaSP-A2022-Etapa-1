e594096e8ea69d04f0a88a12aae2e826b7d0aec5
SantiagoGonzalez97 <santiagogonzalez97@hotmail.com>
2022-09-25 16:11:44 -0300

    BaSP-W06: Html, styles & name of validations sign-up.js

---
+++ Semana-06/javascript/employee-signup.js
@@ -0,0 +1,49 @@
+window.onload = function(){
+//Validation names
+var name = document.getElementById("name");
+console.log("name", name)
+var surname = document.getElementById("surname");
+console.log("surname", surname)
+var dni = document.getElementById("dni");
+console.log("dni", dni);
+var dob = document.getElementById("dob");
+console.log("dob", dob);
+var phone = document.getElementById("phone");
+console.log("phone", phone)
+var address = document.getElementById("address");
+console.log("address", address)
+var location = document.getElementById("location");
+console.log("location", location)
+var postal = document.getElementById("postal");
+console.log("postal", postal);
+var email = document.getElementById("email");
+console.log("email", email)
+var password = document.getElementById("password");
+console.log("password", password);
+var passwordRepeat = document.getElementById("password-repeat");
+console.log("password-repeat", passwordRepeat);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
